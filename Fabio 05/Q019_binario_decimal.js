cf2a26459374f0942450ad51170c8ab558ae31b1
Carlos-henrique-costa <62034240+Carlos-henrique-costa@users.noreply.github.com>
2021-07-06 22:17:18 -0300

    Delete Fabio 05 directory

--- Fabio 05/Q019_binario_decimal.js
+++
@@ -1,9 +0,0 @@
-const prompt = require('prompt-sync')()
-
-function main(){
-    const Binario = Number(prompt('Digite um numero em binaria: '))
-
-    console.log('O numero binario transformado em decimal: ', parseInt(Binario, 2))
-}
-
-main()
